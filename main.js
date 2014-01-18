5e43ba227631fd98acf51be7bfcca37f5e631f4b
lukaramishvili <lukaramishvili@localhost>
2014-01-18 19:21:54 +0400

    married code

--- main.js
+++ main.js
@@ -13,8 +13,57 @@
 //// You should have received a copy of the GNU Affero General Public License
 //// along with Space Audio.  If not, see <http://www.gnu.org/licenses/>.
 
+/*var $logger = $("<div id='logger' />")
+    .css({ position : "absolute", right : 0, bottom : 0,
+	   "background-color" : "gray", padding : 5,
+	 }).appendTo("body");
+
+function log(s) { $logger.html(s); }
+
+function describeFrame(f) {
+    var l = f.gestures.length + " gestures - ";
+    for(var ig in f.gestures) {
+	var g = f.gestures[ig];
+	l += g.state + " " + g.type + ",";
+    }
+    l += "<br>";
+    return l;
+}
+
+function processAction(argset){
+    
+}*/
+
+var xMin = -150;
+var xMax = 150;
+var zOn = 100;
+var zOff = 200;
 
 var leap = zLeap().init(function(frame, prevFrame){
-    
+/*    if(frame.gestures.length > 0){
+	for(var ig in frame.gestures){
+	    var g = frame.gestures[ig];
+	    processAction({ type : "gesture", gesture : g, frame : frame });
+	}
+    }*/
+    for(var nh in frame.hands){
+	var h = frame.hands[nh];
+	for(var nf in h.fingers){
+	    var f = h.fingers[nf];
+	    var x = f.tipPosition[0];
+	    var y = f.tipPosition[1];
+	    var z = f.tipPosition[2];
+	    if(z < zOn){
+		//set drawing cursor
+		//draw();
+	    } else if(z > zOn && z < zOff){
+		//near touchzone, set warning cursor
+	    } else {
+		//not touching, set normal cursor
+	    }
+	    //
+	    console.log(JSON.stringify([x,y,z]));
+	}
+    }
 });
 
---
+++ static/js/script.js
@@ -0,0 +1,142 @@
+navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+navigator.getMedia(
+
+    // constraints
+    {
+        video: true,
+        // audio: true
+    },
+
+    // successCallback
+    function (localMediaStream) {
+        $('#snap').show();
+        var video = document.getElementById("video");
+
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+        // video.onloadedmetadata = function(e) {
+        //    // Do something with the video here.
+        // };
+    },
+
+    // errorCallback
+    function (err) {
+        console.log("The following error occured: " + err);
+        $("#countdown").text("Please Reload and ENABLE your camera");
+    }
+
+    );
+
+function updateCountdown() {
+    var seconds = 4;
+    setTimeout(countdown, 900);
+
+    function countdown() {
+        $('h1').hide();
+        seconds--;
+        if (seconds > 0) {
+            $("#countdown").text(seconds + " seconds... Cheese!");
+            setTimeout(countdown, 900);
+        } else {
+            $("#countdown").text(seconds + " seconds... Cheese!");
+            $("#countdown").hide();
+            video.pause();
+
+            $('#video').hide();
+            $('#canvas').show();
+
+            draw();
+
+            var canvas = document.getElementById("canvas");
+            $("#countdown").text("");
+            $('#download').show();
+            context = canvas.getContext("2d");
+            context.drawImage(video, 0, 0, 640, 480);
+
+
+            $('#snap').prop("disabled", false);
+
+            $("#download").click(function () {
+                var cs = new saver("lib/download.php?id="+video.value);
+                cs.savePNG(canvas);
+            });
+
+        }
+    }
+};
+
+function snap() {
+    $('#canvas').addClass(filters[idx-1]);
+    video.play();
+    $('#snap').attr("disabled", true);
+    updateCountdown();
+    document.querySelector('video').removeEventListener('click', changeFilter , false);
+}
+
+function saver(url) {
+    this.url = url;
+
+    this.savePNG = function (canvas) {
+        if (!canvas || !url) return;
+
+        var data = canvas.toDataURL("image/png");
+        data = data.substr(data.indexOf(',') + 1).toString(); //remove base64
+
+        var dataInput = document.createElement("input");
+        dataInput.setAttribute("name", 'imgdata');
+        dataInput.setAttribute("value", data);
+        dataInput.setAttribute("type", "hidden");
+
+        var form = document.createElement("form");
+        form.method = 'post';
+        form.action = url;
+        form.appendChild(dataInput);
+
+        document.body.appendChild(form);
+        form.submit();
+        document.body.removeChild(form);
+    };
+}
+
+
+
+function draw(){
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var mouse = {x: 0, y: 0};
+    var last_mouse = {x: 0, y: 0};
+
+    /* Mouse Capturing Work */
+    canvas.addEventListener('mousemove', function(e) {
+        last_mouse.x = mouse.x;
+        last_mouse.y = mouse.y;
+
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
+
+
+    /* Drawing on Paint App */
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'blue';
+
+    canvas.addEventListener('mousedown', function(e) {
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+    var onPaint = function() {
+        ctx.beginPath();
+        ctx.moveTo(last_mouse.x, last_mouse.y);
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.closePath();
+        ctx.stroke();
+    };
+}
